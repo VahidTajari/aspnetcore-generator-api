71ea7b3a9f55286e044899f593cfeeff6a5251c1
vahid tajari <v.tajari@gmail.com>
2024-08-25 23:47:08 +0330

    fix test

--- tests/RangeTest.cs
+++ tests/RangeTest.cs
@@ -17,7 +17,7 @@ public class RangeTest
     public void SortShouldOrderResults()
     {
         var range = new Range() { Count = 3, Sort = true };
-        var values = new[] { "a", "b", "c" };
+        var values = new[] { "a", "c", "b" };
         var counter = 0;
         var generated = range.Of(() => values[counter++]).ToArray();
         Assert.Equal(new[] { "a", "b", "c" }, generated);
