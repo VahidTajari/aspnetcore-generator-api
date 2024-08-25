a24d14e742ab3f1a32018afa79d73cc04cc32324
vahid tajari <v.tajari@gmail.com>
2024-08-25 23:43:37 +0330

    add sort test

--- tests/RangeTest.cs
+++ tests/RangeTest.cs
@@ -12,4 +12,14 @@ public class RangeTest
         var generated = range.Of(() => "");
         Assert.Equal(3, generated.Count());
     }
+
+    [Fact]
+    public void SortShouldOrderResults()
+    {
+        var range = new Range() { Count = 3, Sort = true };
+        var values = new[] { "a", "b", "c" };
+        var counter = 0;
+        var generated = range.Of(() => values[counter++]).ToArray();
+        Assert.Equal(new[] { "a", "b", "c" }, generated);
+    }
 }
